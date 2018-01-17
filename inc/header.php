41d42ae2a37c9ef9abd394ac94363ed79e33fbdb
riku6460 <riku6460@users.noreply.github.com>
2018-01-18 01:19:14 +0900

    Twitterカードとかいじった

--- inc/header.php
+++ inc/header.php
@@ -8,12 +8,12 @@
         <link rel="stylesheet" href="/font-awesome/css/font-awesome.min.css">
 
         <meta name="twitter:card" content="summary_large_image">
-        <meta name="twitter:site" content="@nirot1r">
-        <meta name="twitter:creator" content="@nirot1r">
+        <meta name="twitter:site" content="@riku6460">
+        <meta name="twitter:creator" content="@riku6460">
         <meta name="twitter:title" content="Fight for artistic creativity / 自分を守る、みんなで守る。">
         <meta name="twitter:description" content="#このリプ欄にはグロ画像があります 不適切な内容を投稿しているユーザから身を守る方法を提供しています。">
-        <meta name="twitter:image" content="https://ffac.g-second.net/images/card.jpg">
-        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+        <meta name="twitter:image" content="https://ffac.riku.pw/images/card.jpg">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.12.4/jquery.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
   
     </head>
     <body>
